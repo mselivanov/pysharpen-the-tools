9b5f1505cde3c2f4176f7a4bf46be27a678e6bac
maksim_selivanov <maksim.selivanau@iii.com>
2018-02-19 13:25:48 +0300

    Add examples for modulo operator (%) and divmod function.

--- pysharpen/basics/shpbuiltins.py
+++ pysharpen/basics/shpbuiltins.py
@@ -15,11 +15,55 @@ def mathematical_funcs():
                                                 abs_zero_val = abs(zero_val),
                                                 pos_val = pos_val,
                                                 abs_pos_val = abs(pos_val)))
+    print("Modulo operator and floor division equation: x == (x//y)*y + (x%y)")
+    divident, divisor = 5, 3
+    print("{divident} % {divisor} == {result}".format(divident = divident, 
+                                                            divisor = divisor, 
+                                                            result = divident % divisor))
+    divident, divisor = 5, -3
+    print("{divident} % {divisor} == {result}".format(divident = divident, 
+                                                            divisor = divisor, 
+                                                            result = divident % divisor))
+    divident, divisor = -5, 3
+    print("{divident} % {divisor} == {result}".format(divident = divident, 
+                                                            divisor = divisor, 
+                                                            result = divident % divisor))
+    divident, divisor = -5, -3
+    print("{divident} % {divisor} == {result}".format(divident = divident, 
+                                                            divisor = divisor, 
+                                                            result = divident % divisor))
+                                                            
+
+    print("divmod, floor division and modulo equation: x == divmod(x, y) == (x//y, x%y)")
+    divident, divisor = 5, 3
+    quotient, reminder = divmod(divident, divisor)
+    print("divmod({divident}, {divisor}) == {result}".format(divident = divident, 
+                                                            divisor = divisor, 
+                                                            result = divmod(divident, divisor)))
+
+    divident, divisor = 5, -3
+    quotient, reminder = divmod(divident, divisor)
+    print("divmod({divident}, {divisor}) == {result}".format(divident = divident, 
+                                                            divisor = divisor, 
+                                                            result = divmod(divident, divisor)))
+    divident, divisor = -5, 3
+    quotient, reminder = divmod(divident, divisor)
+    print("divmod({divident}, {divisor}) == {result}".format(divident = divident, 
+                                                            divisor = divisor, 
+                                                            result = divmod(divident, divisor)))
+    divident, divisor = -5, -3
+    quotient, reminder = divmod(divident, divisor)
+    print("divmod({divident}, {divisor}) == {result}".format(divident = divident, 
+                                                            divisor = divisor, 
+                                                            result = divmod(divident, divisor)))
+                                                            
+def print_header(message):
+    print("{prefix}{message}{postfix}".format(prefix = "-"*10, 
+                                              message = message, 
+                                              postfix = "-"*10))
 
 def main():
-    print("{prefix}{message}{postfix}".format(prefix = "-"*10, 
-                                              message = "Mathematical functions example", 
-                                              postfix = "-"*10))
+    print_header("Mathematical functions example")
     mathematical_funcs()
 
 if __name__ == '__main__':
