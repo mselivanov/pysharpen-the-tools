59a499d6297b4d61dbd4c822040aa370f65509c5
maksim_selivanov <maksim.selivanau@iii.com>
2018-02-20 15:40:57 +0300

    Add examples with all, any, bool, chr built-ins.

--- pysharpen/basics/shpbuiltins.py
+++ pysharpen/basics/shpbuiltins.py
@@ -62,9 +62,46 @@ def print_header(message):
                                               message = message, 
                                               postfix = "-"*10))
 
+def logical_funcs():
+    logical_constructs = [[], [None], {}, {None}, 0, 0.0, None, "", (), ("",), (None,)]
+    for op in logical_constructs:
+        print("bool({operand}) = {result}".format(operand = op, result = bool(op)))
+    logical_constructs = [[], None, {}, 0, 0.0]
+    print("any({iterable}) = {result}".format(iterable = logical_constructs, result = any(logical_constructs)))
+    logical_constructs = [[], {None}, {}, 0, 0.0, None]
+    print("any({iterable}) = {result}".format(iterable = logical_constructs, result = any(logical_constructs)))
+    logical_constructs = [{None}, [None], 1, "None"]    
+    print("all({iterable}) = {result}".format(iterable = logical_constructs, result = all(logical_constructs)))
+
+def string_char_funcs():
+    s = "Hello, world!"
+    print("ascii({param}) = {result}".format(param = s, result = ascii(s)))
+    print("repr({param}) = {result}".format(param = s, result = repr(s)))
+    
+    s = "This is 'Hello, world!' in Russian: 'Привет, мир!'"
+    print("ascii({param}) = {result}".format(param = s, result = ascii(s)))
+    print("repr({param}) = {result}".format(param = s, result = repr(s)))
+    
+    code_point1 = 97
+    code_point2 = 8364
+    code_point3 = 0x10FFFF
+    print("chr({param}) = {result}".format(param = code_point1, result = chr(code_point1)))
+    print("chr({param}) = {result}".format(param = code_point2, result = chr(code_point2)))
+    print("chr({param}) = {result}".format(param = code_point3, result = chr(code_point3)))
+    try:
+        c1 = chr(code_point3+1)
+    except ValueError as e:
+        print("chr({param}) = 'Exception: {result}'".format(param = code_point3+1, result = e))
+    
+                                              
 def main():
     print_header("Mathematical functions example")
     mathematical_funcs()
+    print_header("Logical functions example")
+    logical_funcs()
+    print_header("String/char functions example")
+    string_char_funcs()
+    
 
 if __name__ == '__main__':
     main()
